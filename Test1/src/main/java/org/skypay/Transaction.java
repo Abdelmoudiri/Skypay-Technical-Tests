c3c69ef4373d00ec52bbac77322e8ca6ca2af75b
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-12-14 11:53:13 +0100

    Add Transaction class to handle transaction details

---
+++ Test1/src/main/java/org/skypay/Transaction.java
@@ -0,0 +1,20 @@
+package org.skypay;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Transaction {
+    private final String date;
+    private final int amount;
+    private final int balance;
+
+    public Transaction(String date, int amount, int balance) {
+        this.date = date;
+        this.amount = amount;
+        this.balance = balance;
+    }
+
+    public String getDate() { return date; }
+    public int getAmount() { return amount; }
+    public int getBalance() { return balance; }
+}
