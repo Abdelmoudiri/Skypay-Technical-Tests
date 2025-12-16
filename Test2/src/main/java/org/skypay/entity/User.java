f8481487f748fb61b96688007e8c2eb40b81aaa6
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-12-16 17:20:50 +0100

    Add User class to represent user entities with id and balance

---
+++ Test2/src/main/java/org/skypay/entity/User.java
@@ -0,0 +1,20 @@
+package org.skypay.entity;
+
+public class User {
+    private int id;
+    private int balance;
+
+    public User(int id, int balance) {
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public int getId() { return id; }
+    public int getBalance() { return balance; }
+    public void setBalance(int balance) { this.balance = balance; }
+    
+    @Override
+    public String toString() {
+        return "User ID: " + id + ", Balance: " + balance;
+    }
+}
